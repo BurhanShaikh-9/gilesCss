e573074e4ee54210eb7c2557f9af037a847bca02
cbghealthplan <85751219+BurhanShaikh-9@users.noreply.github.com>
2025-08-14 01:42:54 +0500

    update

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,44 +1,50 @@
+
+
 $(document).ready(function () {
-    $('.mqscroller').mqScroller({
-        htmlDir: 'ltr',
-        loop: true,
-        direction: 'left',
-        duration: 20000,
-        pauseOnHover: true,
-        gap: 10
-    });
-    
-    
-    
-const playbutton = document.querySelector('.playbutton')
-let video = document.querySelector('.cbgAdvisorySection video')
-let started = false
+ 
+ 
+  $('.mqscroller').mqScroller({
+    htmlDir: 'ltr',
+    loop: true,
+    direction: 'left',
+    duration: 20000,
+    pauseOnHover: true,
+    gap: 10
+  });
 
 
 
-playbutton.addEventListener('click', () => {
+  const playbutton = document.querySelector('.playbutton')
+  let video = document.querySelector('.cbgAdvisorySection video')
+  let started = false
 
-  if(video.paused) {
-    video.play()
-    playbutton.classList.add('playing')
-  } else {
-    video.pause()
+
+
+  playbutton.addEventListener('click', () => {
+
+    if (video.paused) {
+      video.play()
+      playbutton.classList.add('playing')
+    } else {
+      video.pause()
+      playbutton.classList.remove('playing')
+    }
+  })
+
+
+
+
+  video.addEventListener('ended', () => {
     playbutton.classList.remove('playing')
-  }
-})
+  })
 
 
-video.addEventListener('ended', () => {
-  playbutton.classList.remove('playing')  
-})
+  video.addEventListener('play', () => {
+    playbutton.classList.add('playing')
+  })
 
 
-video.addEventListener('play', () => {
-  playbutton.classList.add('playing')  
-})
-
-
-video.addEventListener('pause', () => {
-  playbutton.classList.remove('playing')  
-})
+  video.addEventListener('pause', () => {
+    playbutton.classList.remove('playing')
+  })
 });
